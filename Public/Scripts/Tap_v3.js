4c3d0504f55588d6ee535bde0da7c58b90a1bc98
Alex Ling <22086776+alexling540@users.noreply.github.com>
2021-03-03 23:23:32 -0600

    prevent ghost touches
    this is probably bad practice but oh well lol

--- Public/Scripts/Tap_v3.js
+++ Public/Scripts/Tap_v3.js
@@ -3,9 +3,19 @@
 // @input Component.MaterialMeshVisual infoCard
 // @input int num
 
-script.createEvent("TapEvent").bind(function(eventData) {
+var tapEvent = script.createEvent("TapEvent");
+tapEvent.bind(function(eventData) {
+    print(eventData.getTapPosition());
     if (!global.isEditing && !global.isOpen) {
         global.openInfoCard(script.num);
         print('im clicked');
+    }
+});
+
+script.createEvent("UpdateEvent").bind(function(eventData) {
+   if (global.isEditing || global.isOpen) {
+        tapEvent.enabled = false;
+   } else {
+        tapEvent.enabled = true;
     }
 });
